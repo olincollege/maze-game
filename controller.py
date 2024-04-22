7b2c4c52abf2a92509ed126a26a7806e3dd4a51f
Karis Moon <karis.moon2023@gmail.com>
2024-04-22 16:04:54 -0400

    Write functions for controller class.

---
+++ controller.py
@@ -0,0 +1,37 @@
+"""
+Maze controller.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class MazeController(ABC):
+    """
+    Allows the player to control the status of the board.
+    """
+
+    def __init__(self, board):
+        """
+        Define attributes.
+        """
+        self._board = board
+
+    @property
+    def board(self):
+        """
+        Returns _board.
+        """
+        return self._board
+
+    @abstractmethod
+    def move(self):
+        """
+        Implementations of this method should allow players to control the board.
+        """
+
+
+class PygameController(MazeController):
+    """
+    Extending MazeController to control the status of the TicTacToe board using
+        pygame interface.
+    """
