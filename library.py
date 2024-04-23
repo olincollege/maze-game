1a5a50e01f81135cf4cc87b648f26dbe36817d73
dankhoinguyen <dankhoidk2005@gmail.com>
2024-04-23 13:53:59 -0400

    Added the initial tuples for the level 2 and 3 libraries and worked on the model file

--- library.py
+++ library.py
@@ -1,1 +1,3 @@
 level1_collectibles = {(1, 1): True}
+level2_collectibles = {(1, 1): True}
+level3_collectibles = {(1, 1): True}
--- maze.py
+++ maze.py
@@ -3,6 +3,8 @@ Maze implementation.
 """
 
 from library import level1_collectibles
+from library import level2_collectibles
+from library import level3_collectibles
 
 
 class Maze:
@@ -30,11 +32,20 @@ class Maze:
         """
         if self._level == 1:
             self._collectibles = level1_collectibles
+        if self._level == 2:
+            self._collectibles = level2_collectibles
+        self._collectibles = level3_collectibles
 
     def check_collectible(self, x_location, y_location):
         """
         When a collectible is picked up, remove it from the board and add
         points to the player's score.
+
+        Args:
+            x_location: An integer that represents the x index at which the
+            player is located
+            y_location: An integer that represents the y index at which the
+            player is located
         """
         if (x_location, y_location) in self._collectibles:
             self._collectibles[(x_location, y_location)] = True
@@ -44,8 +55,15 @@ class Maze:
         """
         Locate the file based on the player's level.
         """
+        if self._level == 1:
+            return "Levels/level_1.csv"
+        if self._level == 2:
+            return "Levels/level_2.csv"
+        return "Levels/level_3.csv"
 
     def __repr__(self):
         """
-        Return a string representing the maze.
+        Returns a string with the player location, amount of points, and level
         """
+        location = ()
+        return f"The player location is:{location}\nThe amount of points collected is:{self._score}\nThe current level is:{self._level}"
