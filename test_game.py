9ea8f9505431c2321415c6568b89cd3e9d43f298
Karis Moon <karis.moon2023@gmail.com>
2024-04-30 22:55:55 -0400

    Start adding unit testing

--- test_game.py
+++ test_game.py
@@ -10,52 +10,27 @@ from view import PygameView
 from maze import Maze
 from controller import PygameController
 
-collide_borders_level1_tests = [((200, 200), False), ((300, 500), False)]
+# Define sets of test cases.
+collide_borders_cases = [(1, (200, 200), False), (1, (300, 500), False)]
 
 
+# Define standard testing functions to check functions' outputs given certain
+# inputs defined above.
 @pytest.mark.parametrize(
-    "test_position", "border_collided", collide_borders_level1_tests
+    "level,test_position,border_collided", collide_borders_cases
 )
-def test_collide_borders(test_position, border_collided):
+def test_collide_borders(level, test_position, border_collided):
     """
-    Test the level borders for level 1
+    Test that the game goes back to the start screen
     """
-    # maze = Maze()
-    # view = PygameView(maze)
+    from maze import Maze
+    from library import maps
 
-    timer = pygame.time.Clock()
-
-    maze = Maze()
-    view = PygameView(maze)
-    controller = PygameController(maze)
-
-    run = True
-    while run and not maze.jumpscare():
-        view.background_image("img/test.png")
-        while run:
-            mouse_position = pygame.mouse.get_pos()
-            view.background_image("img/test.png")
-            view.background_image("img/test.png")
-            pygame.display.flip()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-            border_collided = False
-
-        while run:
-            mouse_position = pygame.mouse.get_pos()
-            view.background_image("img/test.png")
-            view.draw_level(level_1)
-            view.character(mouse_position, timer)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-            border_collided = maze.collide_borders()
-    pygame.quit()
-
-    pygame.mouse.set_pos(test_position)
-    result = maze.collide_borders()
+    for _, rectangle in enumerate(maps[level]):
+        rect = rectangle
+        if rect.collidepoint(test_position):
+            result = False
+        else:
+            result = True
 
     assert result == border_collided
